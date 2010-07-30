1ac83339e328ff70e52ce50191f4e9924fdad8b2
Ben Greear <greearb@candelatech.com>
2010-07-30 00:10:16 -0700

    local-bind-fixup:  Fix inet_pton warning.
    Conditionally include <arpa/inet.h> for inet_pton
    headers.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- ares_options.c
+++ ares_options.c
@@ -23,6 +23,10 @@
 #include "ares_private.h"
 #include "inet_net_pton.h"
 
+/* For inet_pton */
+#ifdef HAVE_ARPA_INET_H
+#  include <arpa/inet.h>
+#endif
 
 int ares_get_servers(ares_channel channel,
                      struct ares_addr_node **servers)
