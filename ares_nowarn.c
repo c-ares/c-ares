f9b24e590c2a0ee4ccd2dce09a2398652bc8a2d2
Yang Tse <yangsita@gmail.com>
2011-06-01 12:13:05 +0200

    ares_nowarn: icc 9.1 workaround

--- ares_nowarn.c
+++ ares_nowarn.c
@@ -156,18 +156,26 @@ void aresx_FD_ZERO(fd_set *fdset)
 
 unsigned short aresx_htons(unsigned short usnum)
 {
+#if (__INTEL_COMPILER == 910) && defined(__i386__)
+  return (unsigned short)(((usnum << 8) & 0xFF00) | ((usnum >> 8) & 0x00FF));
+#else
   #pragma warning(push)
   #pragma warning(disable:810) /* conversion may lose significant bits */
   return htons(usnum);
   #pragma warning(pop)
+#endif
 }
 
 unsigned short aresx_ntohs(unsigned short usnum)
 {
+#if (__INTEL_COMPILER == 910) && defined(__i386__)
+  return (unsigned short)(((usnum << 8) & 0xFF00) | ((usnum >> 8) & 0x00FF));
+#else
   #pragma warning(push)
   #pragma warning(disable:810) /* conversion may lose significant bits */
   return ntohs(usnum);
   #pragma warning(pop)
+#endif
 }
 
 #endif /* __INTEL_COMPILER && __unix__ */
