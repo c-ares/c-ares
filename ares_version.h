28354d9ad246ccf99301460c8074d4bcc4adfa8e
Guenter Knauf <lists@gknw.net>
2012-04-22 13:09:39 +0200

    Updated copyright year.

--- ares_version.h
+++ ares_version.h
@@ -3,7 +3,7 @@
 #define ARES__VERSION_H
 
 /* This is the global package copyright */
-#define ARES_COPYRIGHT "2004 - 2011 Daniel Stenberg, <daniel@haxx.se>."
+#define ARES_COPYRIGHT "2004 - 2012 Daniel Stenberg, <daniel@haxx.se>."
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 7
