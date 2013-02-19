b82874ad6f60a9d5a423f8ad0d104ac35953787a
Yang Tse <yangsita@gmail.com>
2013-02-19 13:27:33 +0100

    inet_ntop.c: s/socklen_t/ares_socklen_t for portability

--- inet_ntop.c
+++ inet_ntop.c
@@ -59,13 +59,13 @@ static const char *inet_ntop6(const unsigned char *src, char *dst, size_t size);
  *     Paul Vixie, 1996.
  */
 const char *
-ares_inet_ntop(int af, const void *src, char *dst, socklen_t size)
+ares_inet_ntop(int af, const void *src, char *dst, ares_socklen_t size)
 {
   switch (af) {
   case AF_INET:
-    return (inet_ntop4(src, dst, size));
+    return (inet_ntop4(src, dst, (size_t)size));
   case AF_INET6:
-    return (inet_ntop6(src, dst, size));
+    return (inet_ntop6(src, dst, (size_t)size));
   default:
     SET_ERRNO(EAFNOSUPPORT);
     return (NULL);
