58a822231c0e4599ad9a74d99aecc7e30c41372c
Daniel Stenberg <daniel@haxx.se>
2013-04-23 00:32:51 +0200

    ares_version.h: copyright end range year is now 2013

--- ares_version.h
+++ ares_version.h
@@ -3,7 +3,7 @@
 #define ARES__VERSION_H
 
 /* This is the global package copyright */
-#define ARES_COPYRIGHT "2004 - 2012 Daniel Stenberg, <daniel@haxx.se>."
+#define ARES_COPYRIGHT "2004 - 2013 Daniel Stenberg, <daniel@haxx.se>."
 
 #define ARES_VERSION_MAJOR 1
 #define ARES_VERSION_MINOR 10
