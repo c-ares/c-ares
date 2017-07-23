020119fff37a62ff34b97edd136e8637729e0f73
Konstantinos Sofokleous <ksophocleous@gmail.com>
2017-07-23 09:58:16 +0200

    use CMAKE_CONFIGURATION_TYPES instead of hard coding config names also change CMAKE_BUILD_TYPE to make it work for non IDE generators (Ninja, NMake) remove condition name from ENDIF unset variables when done

--- cmake/msvc_static_runtime.cmake
+++ cmake/msvc_static_runtime.cmake
@@ -1,8 +1,15 @@
-FOREACH (flag_var
-    CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
-    CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
-
+# CMAKE_CONFIGURATION_TYPES is empty on non-IDE generators (Ninja, NMake)
+# and that's why we also use CMAKE_BUILD_TYPE to cover for those generators.
+# For IDE generators, CMAKE_BUILD_TYPE is usually empty
+FOREACH (config_type ${CMAKE_CONFIGURATION_TYPES} ${CMAKE_BUILD_TYPE})
+    STRING (TOUPPER ${config_type} upper_config_type)
+    SET (flag_var "CMAKE_C_FLAGS_${upper_config_type}")
     IF (${flag_var} MATCHES "/MD")
         STRING (REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-    ENDIF (${flag_var} MATCHES "/MD")
+    ENDIF ()
 ENDFOREACH ()
+
+# clean up
+SET (upper_config_type)
+SET (config_type)
+SET (flag_var)
