5d3d2b6b82010e9760c19aef8960d301efa1e780
David Drysdale <drysdale@google.com>
2017-12-13 16:35:40 +0000

    travis: only test in IPv4 mode
    Travis' Trusty environment does not support IPv6.

--- travis/test.sh
+++ travis/test.sh
@@ -6,7 +6,7 @@ if [ "$BUILD_TYPE" != "ios" -a "$BUILD_TYPE" != "analyse" -a "$BUILD_TYPE" != "c
     $TEST_WRAP ./ahost www.google.com
     cd test
     make
-    $TEST_WRAP ./arestest -v $TEST_FILTER
+    $TEST_WRAP ./arestest -4 -v $TEST_FILTER
     ./fuzzcheck.sh
     ./dnsdump  fuzzinput/answer_a fuzzinput/answer_aaaa
     cd ..
