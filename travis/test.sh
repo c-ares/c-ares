58582cd6ae2228e71d23afd1157d9f2418e360e5
Brad House <brad@monetra.com>
2018-02-16 06:49:39 -0500

    travis: standardize CMake test off of Autotools tests
    Instead of running 'make test', run the tests directly like autotools
    does.  It provides more verbose output.

--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if [ "$BUILD_TYPE" != "ios" -a "$BUILD_TYPE" != "analyse" -a "$BUILD_TYPE" != "cmake" ]; then
+if [ "$BUILD_TYPE" != "ios" -a "$BUILD_TYPE" != "analyse" ]; then
     $TEST_WRAP ./adig www.google.com
     $TEST_WRAP ./acountry www.google.com
     $TEST_WRAP ./ahost www.google.com
@@ -11,7 +11,14 @@ if [ "$BUILD_TYPE" != "ios" -a "$BUILD_TYPE" != "analyse" -a "$BUILD_TYPE" != "c
     ./dnsdump  fuzzinput/answer_a fuzzinput/answer_aaaa
     cd ..
 elif [ "$BUILD_TYPE" = "cmake" ] ; then
-    cd cmakebld
-    make test
-    cd ..
+    TESTDIR=../../test/
+    cd cmakebld/bin
+    $TEST_WRAP ./adig www.google.com
+    $TEST_WRAP ./acountry www.google.com
+    $TEST_WRAP ./ahost www.google.com
+    $TEST_WRAP ./arestest -4 -v $TEST_FILTER
+    ./aresfuzz $TESTDIR/fuzzinput/*
+    ./aresfuzzname $TESTDIR/fuzznames/*
+    ./dnsdump $TESTDIR/fuzzinput/answer_a $TESTDIR/fuzzinput/answer_aaaa
+    cd ../..
 fi
