39c73b503d9ef70a58ad1f4a4643f15b01407c66
Brad House <brad@brad-house.com>
2020-11-19 08:18:48 -0500

    Travis: add iOS target built with CMake (#378)
    Issue #377 suggested that CMake builds for iOS with c-ares were broken. This PR adds an automatic Travis build for iOS CMake.
    
    Fix By: Brad House (@bradh352)

--- travis/test.sh
+++ travis/test.sh
@@ -6,7 +6,7 @@ set -e
 [ -z "$TEST_FILTER" ] && export TEST_FILTER="--gtest_filter=-*LiveSearchANY*"
 
 # No tests for ios as it is a cross-compile
-if [ "$BUILD_TYPE" = "ios" ] ; then
+if [ "$BUILD_TYPE" = "ios" -o "$BUILD_TYPE" = "ios-cmake" ] ; then
     exit 0
 fi
 
