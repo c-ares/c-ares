80e95661659f34c90e48d51f7b8df877fc7537aa
bradh352 <brad@brad-house.com>
2021-11-01 15:26:27 -0400

    fix a couple of coveralls vars

--- ci/covupload.sh
+++ ci/covupload.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 if [ "$BUILD_TYPE" = "coverage" ]; then
     echo "CI_NAME=${CI_NAME}"
@@ -6,6 +6,7 @@ if [ "$BUILD_TYPE" = "coverage" ]; then
     echo "CI_BUILD_URL=${CI_BUILD_URL}"
     echo "CI_BRANCH=${CI_BRANCH}"
     echo "CI_PULL_REQUEST=${CI_PULL_REQUEST}"
-    export PATH="$PATH:~/.local/bin"
+    PATH="${PATH}:/root/.local/bin:~/.local/bin"
+    export PATH
     coveralls --gcov-options '\-lp'
 fi
