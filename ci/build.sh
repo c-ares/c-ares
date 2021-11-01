a14f5c55ebee381444a66587b4326bfd0f3139b3
bradh352 <brad@brad-house.com>
2021-11-01 14:59:25 -0400

    make verbose

--- ci/build.sh
+++ ci/build.sh
@@ -13,7 +13,7 @@ if [ "$BUILD_TYPE" != "cmake" -a "$BUILD_TYPE" != "valgrind" -a "$BUILD_TYPE" !=
     mkdir atoolsbld
     cd atoolsbld
     $SCAN_WRAP ../configure --disable-symbol-hiding --enable-expose-statics --enable-maintainer-mode --enable-debug $CONFIG_OPTS
-    $SCAN_WRAP make
+    $SCAN_WRAP make V=1
 else
     if [ "$BUILD_TYPE" = "ios-cmake" ]; then
         mkdir cmakebld
