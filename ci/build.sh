876d5ac77e463c10361c0bb43773fd90987975ef
bradh352 <brad@brad-house.com>
2021-12-20 16:10:05 -0500

    attempt to autobuild for iOS

--- ci/build.sh
+++ ci/build.sh
@@ -8,32 +8,16 @@ elif [ "$CIRRUS_OS" != "" ]; then
     OS="$CIRRUS_OS"
 fi
 
-if [ "$BUILD_TYPE" != "cmake" -a "$BUILD_TYPE" != "valgrind" -a "$BUILD_TYPE" != "ios-cmake" ]; then
+if [ "$BUILD_TYPE" != "cmake" -a "$BUILD_TYPE" != "valgrind" ]; then
     autoreconf -fi
     mkdir atoolsbld
     cd atoolsbld
     $SCAN_WRAP ../configure --disable-symbol-hiding --enable-expose-statics --enable-maintainer-mode --enable-debug $CONFIG_OPTS
     $SCAN_WRAP make
 else
-    if [ "$BUILD_TYPE" = "ios-cmake" ]; then
-        mkdir cmakebld
-        cd cmakebld
-        cmake \
-          -DCMAKE_BUILD_TYPE=DEBUG                 \
-          -DCARES_STATIC=ON                        \
-          -DCARES_STATIC_PIC=ON                    \
-          -DCARES_BUILD_TESTS=OFF                  \
-          -DCMAKE_C_FLAGS=$CFLAGS                  \
-          -DCMAKE_CXX_FLAGS=$CXXFLAGS              \
-          -DCMAKE_OSX_SYSROOT=$SYSROOT             \
-          -DCMAKE_OSX_ARCHITECTURES=$ARCHITECTURES \
-          ..
-        make
-    else
-        # Use cmake for valgrind to prevent libtool script wrapping of tests that interfere with valgrind
-        mkdir cmakebld
-        cd cmakebld
-        cmake -DCMAKE_BUILD_TYPE=DEBUG -DCARES_STATIC=ON -DCARES_STATIC_PIC=ON -DCARES_BUILD_TESTS=ON ..
-        make
-    fi
+    # Use cmake for valgrind to prevent libtool script wrapping of tests that interfere with valgrind
+    mkdir cmakebld
+    cd cmakebld
+    cmake ${CMAKE_FLAGS} ..
+    make
 fi
