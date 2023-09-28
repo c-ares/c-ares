14b6dc3accecd100a816c822dda14cf6c0c85ad9
bradh352 <brad@brad-house.com>
2023-09-28 09:55:08 -0400

    fix

--- src/lib/ares__close_sockets.c
+++ src/lib/ares__close_sockets.c
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) Daniel Stenberg
+ * Copyright (c) The c-ares project and its contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
