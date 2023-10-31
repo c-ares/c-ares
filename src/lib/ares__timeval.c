d7bfe4eef4a3d4928615a67492de08f1862de157
Brad House <brad@brad-house.com>
2023-10-31 13:16:22 -0400

    fix some warnings

--- src/lib/ares__timeval.c
+++ src/lib/ares__timeval.c
@@ -39,8 +39,8 @@ struct timeval ares__tvnow(void)
   */
   struct timeval now;
   DWORD          milliseconds = GetTickCount();
-  now.tv_sec                  = (time_t)milliseconds / 1000;
-  now.tv_usec                 = (time_t)(milliseconds % 1000) * 1000;
+  now.tv_sec                  = (long)milliseconds / 1000;
+  now.tv_usec                 = (long)(milliseconds % 1000) * 1000;
   return now;
 }
 
