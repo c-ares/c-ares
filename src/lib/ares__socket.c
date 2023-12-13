cffde53cd7b2c39a7046a05b841e9821a6be0ef4
Martin Chang <marty1885@users.noreply.github.com>
2023-12-13 21:50:18 +0800

    also apply SOCK_DNS on TCP

--- src/lib/ares__socket.c
+++ src/lib/ares__socket.c
@@ -254,7 +254,7 @@ ares_status_t ares__open_connection(ares_channel_t      *channel,
   ares__llist_node_t       *node;
   int                       type = is_tcp ? SOCK_STREAM : SOCK_DGRAM;
 #ifdef __OpenBSD__
-  if(!is_tcp && server->udp_port == 53) {
+  if(server->udp_port == 53) {
     type |= SOCK_DNS;
   }
 #endif
