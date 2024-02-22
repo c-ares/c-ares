035c4c3776ee0d04a9db97213891367b7998de55
Oliver Welsh <160491261+oliverwelsh@users.noreply.github.com>
2024-02-22 18:34:06 +0000

    Add flag to not use a default local named server on channel initialization (#713)
    Hello, I work on an application for Microsoft which uses c-ares to
    perform DNS lookups. We have made some minor changes to the library over
    time, and would like to contribute these back to the project in case
    they are useful more widely. This PR adds a new channel init flag,
    described below.
    
    Please let me know if I can include any more information to make this PR
    better/easier for you to review. Thanks!
    
    **Summary**
    When initializing a channel with `ares_init_options()`, if there are no
    nameservers available (because `ARES_OPT_SERVERS` is not used and
    `/etc/resolv.conf` is either empty or not available) then a default
    local named server will be added to the channel.
    
    However in some applications a local named server will never be
    available. In this case, all subsequent queries on the channel will
    fail.
    
    If we know this ahead of time, then it may be preferred to fail channel
    initialization directly rather than wait for the queries to fail. This
    gives better visibility, since we know that the failure is due to
    missing servers rather than something going wrong with the queries.
    
    This PR adds a new flag `ARES_FLAG_NO_DFLT_SVR`, to indicate that a
    default local named server should not be added to a channel in this
    scenario. Instead, a new error `ARES_EINITNOSERVER` is returned and
    initialization fails.
    
    **Testing**
    I have added 2 new FV tests:
    - `ContainerNoDfltSvrEmptyInit` to test that initialization fails when
    no nameservers are available and the flag is set.
    - `ContainerNoDfltSvrFullInit` to test that initialization still
    succeeds when the flag is set but other nameservers are available.
    
    Existing FVs are all passing.
    
    **Documentation**
    I have had a go at manually updating the docs to describe the new
    flag/error, but couldn't see any contributing guidance about testing
    this. Please let me know if you'd like anything more here.
    
    ---------
    
    Fix By: Oliver Welsh (@oliverwelsh)

--- include/ares.h
+++ include/ares.h
@@ -161,8 +161,10 @@ typedef enum {
   ARES_ECANCELLED = 24, /* introduced in 1.7.0 */
 
   /* More ares_getaddrinfo error codes */
-  ARES_ESERVICE = 25 /* ares_getaddrinfo() was passed a text service name that
-                      * is not recognized. introduced in 1.16.0 */
+  ARES_ESERVICE = 25, /* ares_getaddrinfo() was passed a text service name that
+                       * is not recognized. introduced in 1.16.0 */
+
+  ARES_ENOSERVER = 26 /* No DNS servers were configured */
 } ares_status_t;
 
 typedef enum {
@@ -196,6 +198,7 @@ typedef enum {
 #define ARES_FLAG_NOALIASES   (1 << 6)
 #define ARES_FLAG_NOCHECKRESP (1 << 7)
 #define ARES_FLAG_EDNS        (1 << 8)
+#define ARES_FLAG_NO_DFLT_SVR (1 << 9)
 
 /* Option mask values */
 #define ARES_OPT_FLAGS           (1 << 0)
