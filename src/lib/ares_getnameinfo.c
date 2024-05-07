2d5203d3608a99b9a1fedac1f6d16ebad773e8f6
Brad House <brad@brad-house.com>
2024-05-07 12:58:42 -0400

    static analyzer warning from 48e8cd2
    Fixes new issue from last commit.

--- src/lib/ares_getnameinfo.c
+++ src/lib/ares_getnameinfo.c
@@ -143,7 +143,7 @@ static void  ares_getnameinfo_int(ares_channel_t        *channel,
         callback(arg, ARES_EBADFLAGS, 0, NULL, NULL);
         return;
       }
-      if (salen == sizeof(struct sockaddr_in6)) {
+      if (sa->sa_family == AF_INET6) {
         ares_inet_ntop(AF_INET6, &addr6->sin6_addr, ipbuf, IPBUFSIZ);
         /* If the system supports scope IDs, use it */
 #ifdef HAVE_STRUCT_SOCKADDR_IN6_SIN6_SCOPE_ID
@@ -159,9 +159,8 @@ static void  ares_getnameinfo_int(ares_channel_t        *channel,
       }
       callback(arg, ARES_SUCCESS, 0, ipbuf, service);
       return;
-    }
-    /* This is where a DNS lookup becomes necessary */
-    else {
+    } else {
+      /* This is where a DNS lookup becomes necessary */
       niquery = ares_malloc(sizeof(struct nameinfo_query));
       if (!niquery) {
         callback(arg, ARES_ENOMEM, 0, NULL, NULL);
