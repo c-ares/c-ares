4939117e27547a53c7543e9041dc484e1efce3d5
Brad House <brad@brad-house.com>
2024-05-26 12:56:32 -0400

    Validate `ares_dns_rr_get_str()` can't return non-valid ASCII strings
    The DNS message protocol when using non-binary strings needs to be in
    the ASCII printable range.  The function prototype does elude to this
    but it was not actually validating the string was in anyway valid and
    could be used.  DNS parsing will now fail if an expected string isn't
    an ASCII string.
    
    Fixes Issue: #769
    Fix By: Brad House (@bradh352)

--- src/lib/ares_str.h
+++ src/lib/ares_str.h
@@ -24,8 +24,8 @@
  *
  * SPDX-License-Identifier: MIT
  */
-#ifndef HEADER_CARES_STRDUP_H
-#define HEADER_CARES_STRDUP_H
+#ifndef ARES_STR_H
+#define ARES_STR_H
 
 #include "ares_setup.h"
 #include "ares.h"
@@ -52,4 +52,20 @@ unsigned char ares__tolower(unsigned char c);
 ares_bool_t   ares__memeq_ci(const unsigned char *ptr, const unsigned char *val,
                              size_t len);
 
-#endif /* HEADER_CARES_STRDUP_H */
+ares_bool_t   ares__isprint(int ch);
+
+/*! Validate the string provided is printable.  The length specified must be
+ *  at least the size of the buffer provided.  If a NULL-terminator is hit
+ *  before the length provided is hit, this will not be considered a valid
+ *  printable string.  This does not validate that the string is actually
+ *  NULL terminated.
+ *
+ *  \param[in] str  Buffer containing string to evaluate.
+ *  \param[in] len  Number of characters to evaluate within provided buffer.
+ *                  If 0, will return TRUE since it did not hit an exception.
+ *  \return ARES_TRUE if the entire string is printable, ARES_FALSE if not.
+ */
+ares_bool_t   ares__str_isprint(const char *str, size_t len);
+
+
+#endif /* __ARES_STR_H */
