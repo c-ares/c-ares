2250c10b46676d9cf02b489a97571ff8262f70f9
Gregor Jasny <gjasny@googlemail.com>
2024-06-08 01:00:50 +0200

    fix some obvious errors reported by the CLion Project Analyzer (#779)
    Fix By: Gregor Jasny (@gjasny)

--- src/lib/ares_getnameinfo.c
+++ src/lib/ares_getnameinfo.c
@@ -82,7 +82,7 @@ static char *lookup_service(unsigned short port, unsigned int flags, char *buf,
                             size_t buflen);
 #ifdef HAVE_SOCKADDR_IN6_SIN6_SCOPE_ID
 static void append_scopeid(const struct sockaddr_in6 *addr6,
-                           unsigned int scopeid, char *buf, size_t buflen);
+                           unsigned int flags, char *buf, size_t buflen);
 #endif
 STATIC_TESTABLE char *ares_striendstr(const char *s1, const char *s2);
 
