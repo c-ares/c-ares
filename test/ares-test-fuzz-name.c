54808a519087182581bea318530afc77ef147251
Brad House <brad@brad-house.com>
2024-06-22 13:02:47 -0400

    fix comments

--- test/ares-test-fuzz-name.c
+++ test/ares-test-fuzz-name.c
@@ -28,15 +28,15 @@
 #include <string.h>
 
 #include "ares.h"
-// Include ares internal file for DNS protocol constants
+/* Include ares internal file for DNS protocol constants */
 #include "ares_nameser.h"
 
 int LLVMFuzzerTestOneInput(const unsigned char *data, unsigned long size);
 
-// Entrypoint for Clang's libfuzzer, exercising query creation.
+/* Entrypoint for Clang's libfuzzer, exercising query creation. */
 int LLVMFuzzerTestOneInput(const unsigned char *data, unsigned long size)
 {
-  // Null terminate the data.
+  /* Null terminate the data. */
   char          *name   = malloc(size + 1);
   unsigned char *buf    = NULL;
   int            buflen = 0;
