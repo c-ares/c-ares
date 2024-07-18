8bfc744c2b853fb480f8d5fa0cf7da4fdda9ad13
Brad House <brad@brad-house.com>
2024-07-18 17:08:35 -0400

    more fixes

--- ci/test.sh
+++ ci/test.sh
@@ -40,7 +40,7 @@ cd "${TESTSBIN}"
 if [ "$TEST_WRAP" != "" ] ; then
   $TEST_WRAP ./arestest -4 $TEST_FILTER
 elif [ "$TEST_DEBUGGER" = "gdb" ] ; then
-  gdb --batch --return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./arestest -4 $TEST_FILTER
+  gdb --batch --batch-silent --return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./arestest -4 $TEST_FILTER
 elif [ "$TEST_DEBUGGER" = "lldb" ] ; then
   lldb --batch -o "run -4 $TEST_FILTER" -k "thread backtrace all" -k "quit 1" ./arestest
 else
