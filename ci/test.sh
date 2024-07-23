2c60d70a423bbd73ac07b8ec3e24563c0d1608e6
Brad House <brad@brad-house.com>
2024-07-23 13:22:12 -0400

    CI: MacOS: move to GitHub Actions instead of Cirrus-CI (#825)
    We've been using a lot of time on Cirrus-CI and our credits run out
    quickly. MacOS costs 15 compute credits vs 3 compute
     credits for Linux. Move MacOS testing to GitHub Actions.
    
    Fix By: Brad House (@bradh352)

--- ci/test.sh
+++ ci/test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Copyright (C) The c-ares project and its contributors
 # SPDX-License-Identifier: MIT
-set -e
+set -e -x -o pipefail
 
 # Travis on MacOS uses CloudFlare's DNS (1.1.1.1/1.0.0.1) which rejects ANY requests.
 # Also, LiveSearchTXT is known to fail on Cirrus-CI on some MacOS hosts, we don't get
 # a truncated UDP response so we never follow up with TCP.
 # Note res_ninit() and /etc/resolv.conf actually have different configs, bad Travis
-[ -z "$TEST_FILTER" ] && export TEST_FILTER="--gtest_filter=-*LiveSearchTXT*:*LiveSearchANY*"
+[ -z "$TEST_FILTER" ] && export TEST_FILTER="-4 --gtest_filter=-*LiveSearchTXT*:*LiveSearchANY*"
 
 # No tests for ios as it is a cross-compile
 if [ "$BUILD_TYPE" = "ios" -o "$BUILD_TYPE" = "ios-cmake" -o "$DIST" = "iOS" ] ; then
@@ -23,20 +23,35 @@ PWD=`pwd`
 TESTDIR="${PWD}/test"
 
 if [ "$BUILD_TYPE" = "autotools" -o "$BUILD_TYPE" = "coverage" ]; then
-    TOOLSBIN="${PWD}/atoolsbld/src/tools"
-    TESTSBIN="${PWD}/atoolsbld/test"
+    TOOLSBIN="${PWD}/atoolsbld/src/tools/.libs/"
+    TESTSBIN="${PWD}/atoolsbld/test/.libs/"
+    export LD_LIBRARY_PATH=${PWD}/atoolsbld/src/lib/.libs:$LD_LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=${PWD}/atoolsbld/src/lib/.libs:$DYLD_LIBRARY_PATH
 else
     TOOLSBIN="${PWD}/cmakebld/bin"
     TESTSBIN="${PWD}/cmakebld/bin"
 fi
 
-GTEST_INSTALL_FAILURE_SIGNAL_HANDLER=1
-export GTEST_INSTALL_FAILURE_SIGNAL_HANDLER
-
 $TEST_WRAP "${TOOLSBIN}/adig" www.google.com
 $TEST_WRAP "${TOOLSBIN}/ahost" www.google.com
 cd "${TESTSBIN}"
-$TEST_WRAP ./arestest -4 $TEST_FILTER
+
+if [ "$TEST_WRAP" != "" ] ; then
+  $TEST_WRAP ./arestest $TEST_FILTER
+elif [ "$TEST_DEBUGGER" = "gdb" ] ; then
+  gdb --batch --batch-silent --return-child-result -ex "handle SIGPIPE nostop noprint pass" -ex "run" -ex "thread apply all bt" -ex "quit" --args ./arestest $TEST_FILTER
+elif [ "$TEST_DEBUGGER" = "lldb" ] ; then
+  # LLDB won't return the exit code of the child process, so we need to extract it from the test output and verify it.
+  lldb --batch -o "settings set target.process.extra-startup-command 'process handle SIGPIPE -n true -p true -s false'" -o "process launch --shell-expand-args 0" -k "thread backtrace all" -k "quit 1" -- ./arestest $TEST_FILTER 2>&1 | tee test_output.txt
+  exit_code=`grep "Process [0-9]* exited with status = [0-9]* (.*)" test_output.txt | sed 's/.* = \([0-9]*\).*/\1/'`
+  echo "Test Exit Code: ${exit_code}"
+  if [ "${exit_code}" != "0" ] ; then
+    exit 1
+  fi
+else
+  ./arestest $TEST_FILTER
+fi
+
 ./aresfuzz ${TESTDIR}/fuzzinput/*
 ./aresfuzzname ${TESTDIR}/fuzznames/*
 ./dnsdump "${TESTDIR}/fuzzinput/answer_a" "${TESTDIR}/fuzzinput/answer_aaaa"
