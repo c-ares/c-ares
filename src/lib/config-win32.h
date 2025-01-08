99fc7cea09b5932479c0a0220a0e11c50c86884f
Brad House <brad@brad-house.com>
2025-01-08 12:32:16 -0500

    for non-cmake/autotools builds, only enable threads if _WIN32_WINNT >= 0x0600

--- src/lib/config-win32.h
+++ src/lib/config-win32.h
@@ -237,8 +237,10 @@
 #  undef HAVE_NETIOAPI_H
 #endif
 
-/* Threading support enabled */
-#define CARES_THREADS 1
+/* Threading support enabled for Vista+ */
+#if !defined(_WIN32_WINNT) || _WIN32_WINNT >= 0x0600
+#  define CARES_THREADS 1
+#endif
 
 /* ---------------------------------------------------------------- */
 /*                       TYPEDEF REPLACEMENTS                       */
