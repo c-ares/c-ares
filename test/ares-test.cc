0257b73cf29c2fef00b04262435f26d6bdd29308
Brad House <brad@brad-house.com>
2025-04-08 07:24:04 -0400

    test build fix

--- test/ares-test.cc
+++ test/ares-test.cc
@@ -346,10 +346,12 @@ void LibraryTest::SetFailSend() {
 }
 
 #ifndef HAVE_WRITEV
+extern "C" {
 /* Structure for scatter/gather I/O. */
 struct iovec {
   void  *iov_base; /* Pointer to data. */
   size_t iov_len;  /* Length of data.  */
+};
 };
 #endif
 
